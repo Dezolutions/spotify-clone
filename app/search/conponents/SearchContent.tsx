babf6f37158530e2d6d8d4ca94c423154b0dcc09
Dezolutions <mars92851@gmail.com>
2023-08-03 13:38:24 +0300

    added search page and like button feature

---
+++ app/search/conponents/SearchContent.tsx
@@ -0,0 +1,33 @@
+"use client"
+
+import LibraryItem from "@/components/LibraryItem"
+import LikeButton from "@/components/LikeButton"
+import { Song } from "@/types"
+
+interface SearchContentProps {
+  songs: Song[]
+}
+const SearchContent: React.FC<SearchContentProps> = ({songs}) => {
+
+  if(!songs) {
+    return (
+      <p className="flex flex-col gap-y-2 w-full px-6 text-neutral-400">
+        No songs found.
+      </p>
+    )
+  }
+  return (
+    <div className="flex flex-col gap-y-2 px-6 w-full">
+      {songs.map(song => (
+        <div key={song.id} className="flex items-center gap-x-4 w-full">
+          <div className="flex-1">
+            <LibraryItem data={song} onClick={() => {}}/>
+          </div>
+          <LikeButton songId={song.id}/>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default SearchContent
